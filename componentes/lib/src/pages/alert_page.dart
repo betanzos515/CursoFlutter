0b078c37b10e859a5d943c281e74aef1207f428e
David Betanzos <betanzos515@gmail.com>
2020-03-06 16:50:13 -0600

    Avances en el proyecto de Componentes

---
+++ componentes/lib/src/pages/alert_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AlertPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Avatar Page'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.folder_open),
+        onPressed: (){
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+}
--- componentes/lib/src/pages/home_page.dart
+++ componentes/lib/src/pages/home_page.dart
@@ -1,5 +1,9 @@
-import 'package:componentes/src/pages/providers/menu_provider.dart';
+
+import 'package:componentes/src/providers/menu_provider.dart';
+import 'package:componentes/src/utils/icono_string_util.dart';
 import 'package:flutter/material.dart';
+
+import 'alert_page.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -14,19 +18,42 @@ class HomePage extends StatelessWidget {
 
   Widget _lista() {
 
-    menuProvider.cargarData();
-    return ListView(
-      children: _listaItems(),
+    //Instancia de la clase menuProvider
+    //print(menuProvider.opciones);
+
+    return FutureBuilder(
+      future:menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot <List<dynamic>> snapshot){
+        return ListView(
+          children: _listaItems(snapshot.data,context),
+        );
+      },
     );
   }
 
-  List<Widget> _listaItems(){
-    return [
-      /* ListTile(title: Text('Hola Mundo'),),
-      Divider(),
-      ListTile(title: Text('Hola Mundo'),),
-      Divider(),
-      ListTile(title: Text('Hola Mundo'),) */
-    ];
+  List<Widget> _listaItems(List<dynamic> data,BuildContext context){
+    final List<Widget> opciones = [];
+
+    data.forEach((opc){
+      final widgetTemp = ListTile(
+        title: Text(opc['texto']),
+        leading: getIcon(opc['icon']),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.cyan),
+        onTap: (){
+          final route = MaterialPageRoute(
+            builder: (context){
+              return AlertPage();
+            }
+          );
+          Navigator.push(context,route);
+        },
+      );
+      opciones..add(widgetTemp)
+              ..add(Divider());
+    });
+
+    return opciones;
   } 
-}
+
+}/* Cierre clase */
